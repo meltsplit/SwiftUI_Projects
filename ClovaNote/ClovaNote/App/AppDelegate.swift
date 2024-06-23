fa7d5856071bf10b0027a185f7ceee9f6b0571b0
Melt <seokwoo2000@naver.com>
2024-06-24 05:14:55 +0900

    3-2-2 프로젝트 생성

---
+++ ClovaNote/ClovaNote/App/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  ClovaNote
+//
+//  Created by 장석우 on 6/24/24.
+//
+
+import Foundation
---
+++ ClovaNote/ClovaNote/App/ClovaNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClovaNoteApp.swift
+//  ClovaNote
+//
+//  Created by 장석우 on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct ClovaNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
