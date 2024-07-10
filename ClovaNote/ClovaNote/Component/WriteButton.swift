cce6d139b2a22607153212ac70acd7fc58e8c37c
Melt <seokwoo2000@naver.com>
2024-07-10 19:51:18 +0900

    3-9-2 커스텀뷰, 뷰 modifier 구현하기

---
+++ ClovaNote/ClovaNote/Component/WriteButton.swift
@@ -0,0 +1,105 @@
+//
+//  WriteButton.swift
+//  ClovaNote
+//
+//  Created by 장석우 on 7/10/24.
+//
+
+import SwiftUI
+
+
+public struct WriteButtonViewModifier: ViewModifier {
+  let action: () -> Void
+  
+  public init(action: @escaping () -> Void) {
+    self.action = action
+  }
+  
+  public func body(content: Content) -> some View {
+    ZStack {
+      content
+      
+      VStack {
+        Spacer()
+        HStack {
+          Spacer()
+          Button(
+            action: action,
+            label: { Image(.writeBtn)}
+          )
+        }
+        
+      }
+      .padding(.trailing, 20)
+      .padding(.bottom, 50)
+      
+    }
+    
+  }
+}
+
+//MARK: - 2
+
+extension View {
+  public func writeButton(perform action: @escaping () -> Void) -> some View {
+    ZStack {
+      
+      self
+      
+      VStack {
+        Spacer()
+        HStack {
+          Spacer()
+          Button(
+            action: action,
+            label: { Image(.writeBtn)}
+          )
+        }
+        
+      }
+      .padding(.trailing, 20)
+      .padding(.bottom, 50)
+    }
+    
+  }
+}
+
+
+//MARK: - 3
+
+private struct WriteTodoButtonView<Content: View>: View {
+  let content: Content
+  let action: () -> Void
+  
+  public init(
+    @ViewBuilder content: () -> Content,
+    action: @escaping () -> Void) {
+    self.content = content()
+    self.action = action
+  }
+
+  public var body: some View {
+    ZStack {
+      
+      content
+      
+      VStack {
+        Spacer()
+        HStack {
+          Spacer()
+          Button(
+            action: action,
+            label: { Image(.writeBtn)}
+          )
+        }
+        
+      }
+      .padding(.trailing, 20)
+      .padding(.bottom, 50)
+    }
+    
+  }
+
+  
+}
+
